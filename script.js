0cb37f6b8fb56991cd486d4b654e9efaf32a89d2
protecting the user password keys at rest <sreekanthkavali3@gmail.com>
2024-10-23 17:39:20 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    if (name && email && message) {
+        alert(`Thank you, ${name}! We will contact you soon.`);
+        document.getElementById('contactForm').reset(); // Reset the form
+    } else {
+        alert('Please fill in all fields.');
+    }
+});
